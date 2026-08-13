73cb0f317fdb34894bef4289c630b3814d92589c
djotanas <sennasjonatas@gmail.com>
2026-08-13 10:13:22 -0300

    add app_theme and main config

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/core/theme/app_colors.dart';
+import 'package:todo_app/core/theme/app_text_styles.dart';
+
+abstract class AppTheme {
+  AppTheme._();
+
+  static const _typoGraphyConfig = TextTheme(
+    displayLarge: AppTextStyles.headlineXL,
+    displayMedium: AppTextStyles.headlineL,
+    displaySmall: AppTextStyles.headlineM,
+    bodyMedium: AppTextStyles.bodyL,
+    labelMedium: AppTextStyles.labelM,
+  );
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      brightness: Brightness.light,
+      scaffoldBackgroundColor: AppColors.light.neutral,
+      colorScheme: ColorScheme.light(
+        primary: AppColors.light.primary,
+        secondary: AppColors.light.secondary,
+        tertiary: AppColors.light.tertiary,
+        surface: AppColors.light.neutral,
+      ),
+      textTheme: _typoGraphyConfig,
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: AppColors.dark.neutral,
+      colorScheme: ColorScheme.dark(
+        primary: AppColors.dark.primary,
+        secondary: AppColors.dark.secondary,
+        tertiary: AppColors.dark.tertiary,
+        surface: AppColors.dark.neutral,
+      ),
+      textTheme: _typoGraphyConfig,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/core/theme/app_theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      // ignore: avoid_redundant_argument_values
+      themeMode: ThemeMode.system,
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
