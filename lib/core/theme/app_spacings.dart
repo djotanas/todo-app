6b55679dcff9a7a168b7a8a037aa4d8ce7fe329f
djotanas <sennasjonatas@gmail.com>
2026-08-14 17:18:51 -0300

    add spacing theme config

---
+++ lib/core/theme/app_spacings.dart
@@ -0,0 +1,22 @@
+/// Theme constant for spacing values.
+abstract class AppSpacings {
+  const AppSpacings._();
+
+  /// Extra small spacing value (4px).
+  static const double xs = 4;
+
+  /// Small spacing value (8px).
+  static const double sm = 8;
+
+  /// Medium spacing value (16px).
+  static const double md = 16;
+
+  /// Large spacing value (24px).
+  static const double lg = 24;
+
+  /// Extra large spacing value (32px).
+  static const double xl = 32;
+
+  /// Extra extra large spacing value (48px).
+  static const double xxl = 48;
+}
