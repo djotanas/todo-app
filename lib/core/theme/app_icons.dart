08dedf72e89f761abc9f4d37b9967e002f6943d3
djotanas <sennasjonatas@gmail.com>
2026-08-14 17:18:16 -0300

    add icons theme config

---
+++ lib/core/theme/app_icons.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+/// App icons
+abstract class AppIcons {
+  AppIcons._();
+
+  /// List of checklists icon.
+  static const IconData checkList = Icons.checklist_outlined;
+
+  /// History icon.
+  static const IconData history = Icons.history_outlined;
+
+  /// Settings icon.
+  static const IconData settings = Icons.settings_outlined;
+
+  /// Add icon.
+  static const IconData add = Icons.add_outlined;
+
+  /// Check box blank icon.
+  static const IconData checkBoxBlank = Icons.check_box_outline_blank;
+
+  /// Check box checked icon.
+  static const IconData checkBoxChecked = Icons.check_box;
+}
